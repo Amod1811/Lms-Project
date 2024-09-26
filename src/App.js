cb24e3dd4886181969e3779e991f21c9caeea698
tijareshubhangi <shubhangitijare01@gmail.com>
2024-09-26 17:14:54 +0530

    nav

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Nav from './Component/Nav';
 import Nav1 from './Component/Nav1';
+import Login from './Component/Login';
 
 function App() {
   return (
     <>
      <Nav/>
+      <Login/>
     <Nav1/>
     </>
   );
