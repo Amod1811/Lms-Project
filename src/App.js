82f75cc3218d115de712938c97a7fad4f6af3030
KhushiDiwan16 <diwankhushi1234@gmail.com>
2024-09-26 16:58:26 +0530

    edits

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Nav from './Component/Nav';
+import Nav1 from './Component/Nav1';
 
 function App() {
   return (
     <>
      <Nav/>
-
+    <Nav1/>
     </>
   );
 }
