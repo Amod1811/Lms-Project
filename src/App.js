6192d9f4e8e8aebf48ea7941b2170b745d536b9b
Aditya04tech <aditya.systemengineer@gmail.com>
2024-09-27 16:41:42 +0530

    home update

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Nav from './Component/Nav';
 import Nav1 from './Component/Nav1';
 import Login from './Component/Login';
 import SignUp from './Component/SignUp';
+import Footer from './Component/Footer';
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
      
       
     <Nav1/>
-   
+   <Footer/>
     </>
   );
 }
