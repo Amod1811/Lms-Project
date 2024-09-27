681a3de88f8345ec118d035e625bbe81ee851fba
KhushiDiwan16 <diwankhushi1234@gmail.com>
2024-09-27 14:17:06 +0530

    ed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Nav from './Component/Nav';
 import Nav1 from './Component/Nav1';
--- src/Component/Nav1.js
+++ src/Component/Nav1.js
@@ -29,7 +29,7 @@ const Nav1 = () => {
 
       <ul className="nav navbar-nav navbar-right">
         <li>
-            <img src="" alt="" />
+            <img src="../user 1.png" alt="" />
         </li>
         <li>
             <img src="" alt="" />
@@ -37,7 +37,6 @@ const Nav1 = () => {
       </ul>
     </div>
   </nav>
-  
 </div> 
   )
 }
